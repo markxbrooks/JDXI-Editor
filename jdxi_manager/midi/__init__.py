d20f66613b77377c170e2860bd654a6021567d00
Mark Brooks <Mark.x.brooks@gmail.com>
2025-01-15 09:25:01 -0500

    effects

--- jdxi_manager/midi/__init__.py
+++ jdxi_manager/midi/__init__.py
@@ -1,4 +1,13 @@
 from .helper import MIDIHelper
 from .connection import MIDIConnection
+from .constants import analog, digital, drums, sysex, vocal
 
-__all__ = ['MIDIHelper', 'MIDIConnection'] 
+__all__ = [
+    'MIDIHelper', 
+    'MIDIConnection',
+    'analog',
+    'digital', 
+    'drums',
+    'sysex',
+    'vocal'
+] 
--- jdxi_manager/midi/constants/analog.py
+++ jdxi_manager/midi/constants/analog.py
@@ -1,51 +1,9 @@
 """Analog synth MIDI constants"""
 
 from enum import Enum, IntEnum
+from .sysex import ANALOG_SYNTH_AREA, ANALOG_PART  # Import from sysex.py
 
-# Areas and Parts
-ANALOG_SYNTH_AREA = 0x01
-ANALOG_PART = 0x00
-
-# Analog Oscillator Parameters
-ANALOG_OSC_WAVE = 0x16        # Waveform (0-2: SAW,TRI,PW-SQR)
-ANALOG_OSC_COARSE = 0x17      # Pitch Coarse (40-88: -24 to +24)
-ANALOG_OSC_FINE = 0x18        # Pitch Fine (14-114: -50 to +50)
-ANALOG_OSC_PW = 0x19          # Pulse Width (0-127)
-ANALOG_OSC_PWM = 0x1A         # PW Mod Depth (0-127)
-ANALOG_OSC_PENV_VELO = 0x1B   # Pitch Env Velocity (1-127: -63 to +63)
-ANALOG_OSC_PENV_A = 0x1C      # Pitch Env Attack (0-127)
-ANALOG_OSC_PENV_D = 0x1D      # Pitch Env Decay (0-127)
-ANALOG_OSC_PENV_DEPTH = 0x1E  # Pitch Env Depth (1-127: -63 to +63)
-ANALOG_SUB_TYPE = 0x1F        # Sub Osc Type (0-2: OFF,OCT-1,OCT-2)
-
-# Analog Synth Parameters
-ANALOG_FILTER_CUTOFF = 0x21  # Filter cutoff frequency (0-127)
-ANALOG_FILTER_RESONANCE = 0x23  # Filter resonance (0-127)
-ANALOG_AMP_LEVEL = 0x2A  # Amplifier level (0-127)
-ANALOG_LFO_SHAPE = 0x30  # LFO waveform shape (0-5)
-ANALOG_LFO_RATE = 0x31  # LFO rate (0-127)
-
-# Analog Synth Areas and Parts
-ANALOG_SYNTH_AREA = 0x1B    # Analog synth area
-ANALOG_PART = 0x01          # Analog synth part
-
-# Analog Synth Parameter Groups
-ANALOG_OSC_GROUP = 0x00     # Oscillator parameters
-ANALOG_FILTER_GROUP = 0x01  # Filter parameters
-ANALOG_AMP_GROUP = 0x02     # Amplifier parameters
-ANALOG_LFO_GROUP = 0x03     # LFO parameters
-
-# Analog LFO Parameters
-ANALOG_LFO_SHAPE = 0x0D        # LFO Shape (0-5: TRI,SIN,SAW,SQR,S&H,RND)
-ANALOG_LFO_RATE = 0x0E         # LFO Rate (0-127)
-ANALOG_LFO_FADE = 0x0F         # LFO Fade Time (0-127)
-ANALOG_LFO_SYNC = 0x10         # LFO Tempo Sync Switch (0-1)
-ANALOG_LFO_SYNC_NOTE = 0x11    # LFO Tempo Sync Note (0-19)
-ANALOG_LFO_PITCH = 0x12        # LFO Pitch Depth (1-127: -63 to +63)
-ANALOG_LFO_FILTER = 0x13       # LFO Filter Depth (1-127: -63 to +63)
-ANALOG_LFO_AMP = 0x14          # LFO Amp Depth (1-127: -63 to +63)
-ANALOG_LFO_KEY_TRIG = 0x15     # LFO Key Trigger (0-1)
-
+# Control Change Parameters
 class AnalogToneCC(IntEnum):
     """Analog synth CC parameters"""
     # Oscillator Parameters
@@ -77,26 +35,21 @@ class AnalogToneCC(IntEnum):
     AMP_ENV_R = 0x2E       # Amplifier envelope release (0-127)
     
     # LFO parameters
-    LFO_SHAPE = 0x0D
-    LFO_RATE = 0x0E
-    LFO_FADE = 0x0F
-    LFO_SYNC = 0x10
-    LFO_SYNC_NOTE = 0x11
-    LFO_PITCH = 0x12
-    LFO_FILTER = 0x13
-    LFO_AMP = 0x14
-    LFO_KEY_TRIG = 0x15
+    LFO_SHAPE = 0x0D       # LFO Shape (0-5: TRI,SIN,SAW,SQR,S&H,RND)
+    LFO_RATE = 0x0E        # LFO Rate (0-127)
+    LFO_FADE = 0x0F        # LFO Fade Time (0-127)
+    LFO_SYNC = 0x10        # LFO Tempo Sync Switch (0-1)
+    LFO_SYNC_NOTE = 0x11   # LFO Tempo Sync Note (0-19)
+    LFO_PITCH = 0x12       # LFO Pitch Depth (1-127: -63 to +63)
+    LFO_FILTER = 0x13      # LFO Filter Depth (1-127: -63 to +63)
+    LFO_AMP = 0x14         # LFO Amp Depth (1-127: -63 to +63)
+    LFO_KEY_TRIG = 0x15    # LFO Key Trigger (0-1)
 
 class Waveform(Enum):
     """Analog oscillator waveform types"""
     SAW = 0
     TRIANGLE = 1
     PULSE = 2  # Changed from SQUARE to PULSE to match JD-Xi terminology
-
-    @property
-    def midi_value(self) -> int:
-        """Get MIDI value for waveform"""
-        return self.value
 
     @property
     def display_name(self) -> str:
@@ -108,25 +61,23 @@ class Waveform(Enum):
         }
         return names.get(self.value, "???")
 
-# Sub oscillator types  
-class AnalogSubType(Enum):
+class SubOscType(Enum):
     """Analog sub oscillator types"""
     OFF = 0
     OCT_DOWN_1 = 1  # -1 octave
     OCT_DOWN_2 = 2  # -2 octaves
 
-    @staticmethod
-    def get_display_name(value: int) -> str:
+    @property
+    def display_name(self) -> str:
         """Get display name for sub oscillator type"""
         names = {
             0: "OFF",
             1: "-1 OCT",
             2: "-2 OCT"
         }
-        return names.get(value, "???")
+        return names.get(self.value, "???")
 
-# Analog LFO Shape Values
-class AnalogLFOShape(Enum):
+class LFOShape(Enum):
     """Analog LFO waveform shapes"""
     TRIANGLE = 0    # Triangle wave
     SINE = 1        # Sine wave
@@ -135,8 +86,8 @@ class AnalogLFOShape(Enum):
     SAMPLE_HOLD = 4 # Sample & Hold
     RANDOM = 5      # Random
 
-    @staticmethod
-    def get_display_name(value: int) -> str:
+    @property
+    def display_name(self) -> str:
         """Get display name for LFO shape"""
         names = {
             0: "TRI",
@@ -146,10 +97,23 @@ class AnalogLFOShape(Enum):
             4: "S&H",
             5: "RND"
         }
-        return names.get(value, "???")
+        return names.get(self.value, "???")
 
-# Analog LFO Sync Note Values
-ANALOG_LFO_SYNC_NOTES = [
+# Parameter value ranges
+LFO_RANGES = {
+    'shape': (0, 5),
+    'rate': (0, 127),
+    'fade': (0, 127),
+    'sync': (0, 1),
+    'sync_note': (0, 19),
+    'pitch': (-63, 63),
+    'filter': (-63, 63),
+    'amp': (-63, 63),
+    'key_trig': (0, 1)
+}
+
+# LFO Sync Note Values
+LFO_SYNC_NOTES = [
     "16",    # 0
     "12",    # 1
     "8",     # 2
@@ -171,16 +135,3 @@ ANALOG_LFO_SYNC_NOTES = [
     "1/24",  # 18
     "1/32"   # 19
 ]
-
-# Parameter value ranges
-ANALOG_LFO_RANGES = {
-    'shape': (0, 5),
-    'rate': (0, 127),
-    'fade': (0, 127),
-    'sync': (0, 1),
-    'sync_note': (0, 19),
-    'pitch': (-63, 63),
-    'filter': (-63, 63),
-    'amp': (-63, 63),
-    'key_trig': (0, 1)
-}
