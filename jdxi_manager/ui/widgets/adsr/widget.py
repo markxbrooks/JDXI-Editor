6ca576ba1cf49f453a57193e910f5166801e2613
Mark Brooks <Mark.x.brooks@gmail.com>
2025-02-22 11:57:34 -0500

    Revert "Revert "new ADSR widget""
    This reverts commit e1d0852396c89c2e50ee21479795db37177899a1.

--- jdxi_manager/ui/widgets/adsr/widget.py
+++ jdxi_manager/ui/widgets/adsr/widget.py
@@ -4,9 +4,10 @@ Editing ADSR parameters
 """
 
 from PySide6.QtCore import Signal
-from PySide6.QtWidgets import QWidget, QLabel, QSpinBox, QDoubleSpinBox, QGridLayout
+from PySide6.QtWidgets import QWidget, QLabel, QSpinBox, QDoubleSpinBox, QGridLayout, QHBoxLayout
 
 from jdxi_manager.ui.widgets.adsr.plot import ADSRPlot
+from jdxi_manager.ui.style import Style
 
 
 class ADSRWidget(QWidget):
@@ -15,77 +16,64 @@ class ADSRWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.envelope = {
-            "attackTime": 100,
-            "decayTime": 400,
-            "releaseTime": 100,
-            "initialAmpl": 0,
-            "peakAmpl": 1,
-            "sustainAmpl": 0.8,
+            "attack_time": 100,
+            "decay_time": 400,
+            "release_time": 100,
+            "initial_level": 0,
+            "peak_level": 1,
+            "sustain_level": 0.8,
         }
         self.setMinimumHeight(150)  # Adjust height as needed
         self.attack_sb = self.create_spinbox(
-            0, 1000, " ms", self.envelope["attackTime"]
+            0, 1000, " ms", self.envelope["attack_time"]
         )
-        self.decay_sb = self.create_spinbox(0, 1000, " ms", self.envelope["decayTime"])
+        self.decay_sb = self.create_spinbox(0, 1000, " ms", self.envelope["decay_time"])
         self.release_sb = self.create_spinbox(
-            0, 1000, " ms", self.envelope["releaseTime"]
+            0, 1000, " ms", self.envelope["release_time"]
         )
-        self.initial_sb = self.create_double_spinbox(
-            0, 1, 0.01, self.envelope["initialAmpl"]
-        )
-        self.peak_sb = self.create_double_spinbox(0, 1, 0.01, self.envelope["peakAmpl"])
+        #self.initial_sb = self.create_double_spinbox(
+        #    0, 1, 0.01, self.envelope["initial_level"]
+        #)
+        # self.peak_sb = self.create_double_spinbox(0, 1, 0.01, self.envelope["peak_level"])
         self.sustain_sb = self.create_double_spinbox(
-            0, 1, 0.01, self.envelope["sustainAmpl"]
+            0, 1, 0.01, self.envelope["sustain_level"]
         )
-        self.setStyleSheet(
-            """
-                    QSpinBox {
-                           font-family: Myriad Pro, sans-serif;
-                           font-size: 10px;
-                    }
-                           QDoubleSpinBox {
-                           font-family: Myriad Pro, sans-serif;
-                           font-size: 10px;
-                    }
-                    QSlider::groove:vertical {
-                        background: red;
-                        width: 6px;
-                        border-radius: 3px;
-                    }
-                    
-                    QSlider::handle:vertical {
-                        background: gray;
-                        border: 1px solid darkgray;
-                        width: 14px;
-                        height: 14px;
-                        margin: -6px 0;
-                        border-radius: 7px;
-                    }
-                """
-        )
+        self.setStyleSheet(Style.EDITOR_STYLE)
         self.plot = ADSRPlot()
 
+        self.sliders = QWidget()
+        self.sliders_layout = QHBoxLayout(self.sliders)
         self.layout = QGridLayout(self)
-        self.layout.addWidget(QLabel("Attack:"), 0, 0)
-        self.layout.addWidget(self.attack_sb, 0, 1)
-        self.layout.addWidget(QLabel("Decay:"), 1, 0)
-        self.layout.addWidget(self.decay_sb, 1, 1)
-        self.layout.addWidget(QLabel("Release:"), 2, 0)
-        self.layout.addWidget(self.release_sb, 2, 1)
-        self.layout.addWidget(QLabel("Initial:"), 0, 2)
-        self.layout.addWidget(self.initial_sb, 0, 3)
-        self.layout.addWidget(QLabel("Peak:"), 1, 2)
-        self.layout.addWidget(self.peak_sb, 1, 3)
-        self.layout.addWidget(QLabel("Sustain:"), 2, 2)
-        self.layout.addWidget(self.sustain_sb, 2, 3)
+
+        # self.layout.addWidget(QLabel("Attack:"), 0, 0)
+        # self.layout.addWidget(self.attack_sb, 0, 1)
+        # self.layout.addWidget(QLabel("Decay:"), 1, 0)
+        # self.layout.addWidget(self.decay_sb, 1, 1)
+        # self.layout.addWidget(QLabel("Release:"), 2, 0)
+        # self.layout.addWidget(self.release_sb, 2, 1)
+        # self.layout.addWidget(QLabel("Initial:"), 0, 2)
+        # self.layout.addWidget(self.initial_sb, 0, 3)
+        # self.layout.addWidget(QLabel("Peak:"), 1, 2)
+        # self.layout.addWidget(self.peak_sb, 1, 3)
+        # self.layout.addWidget(QLabel("Sustain:"), 2, 2)
+        # self.layout.addWidget(self.sustain_sb, 2, 3)
+
+        self.layout.addWidget(self.attack_sb, 0, 0)
+        self.layout.addWidget(self.decay_sb, 0, 1)
+        self.layout.addWidget(self.release_sb, 0, 2)
+        self.layout.addWidget(self.sustain_sb, 0, 3)
+
+        # self.layout.addWidget(self.plot, 3, 4, 4, 1)
+        self.layout.addLayout(self.sliders_layout, 1, 0, 4, 3)
         self.layout.addWidget(self.plot, 0, 4, 4, 1)
+
         self.layout.setColumnMinimumWidth(4, 150)
 
         self.attack_sb.valueChanged.connect(self.valueChanged)
         self.decay_sb.valueChanged.connect(self.valueChanged)
         self.release_sb.valueChanged.connect(self.valueChanged)
-        self.initial_sb.valueChanged.connect(self.valueChanged)
-        self.peak_sb.valueChanged.connect(self.valueChanged)
+        # self.initial_sb.valueChanged.connect(self.valueChanged)
+        # self.peak_sb.valueChanged.connect(self.valueChanged)
         self.sustain_sb.valueChanged.connect(self.valueChanged)
 
         self.setLayout(self.layout)
@@ -106,11 +94,13 @@ class ADSRWidget(QWidget):
         return sb
 
     def valueChanged(self):
-        self.envelope["attackTime"] = self.attack_sb.value()
-        self.envelope["decayTime"] = self.decay_sb.value()
-        self.envelope["releaseTime"] = self.release_sb.value()
-        self.envelope["initialAmpl"] = self.initial_sb.value()
-        self.envelope["peakAmpl"] = self.peak_sb.value()
-        self.envelope["sustainAmpl"] = self.sustain_sb.value()
+        self.envelope["attack_time"] = self.attack_sb.value()
+        self.envelope["decay_time"] = self.decay_sb.value()
+        self.envelope["release_time"] = self.release_sb.value()
+        # self.envelope["initial_level"] = self.initial_sb.value()
+        # self.envelope["peak_level"] = self.peak_sb.value()
+        self.envelope["sustain_level"] = self.sustain_sb.value()
         self.plot.set_values(self.envelope)
         self.envelopeChanged.emit(self.envelope)
+
+
