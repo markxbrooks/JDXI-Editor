c66fc0fcc7bdf72c6d8062fa4937574261db19c9
Mark Brooks <Mark.x.brooks@gmail.com>
2025-03-14 22:16:38 -0400

    added PresetData type

--- jdxi_editor/midi/preset/type.py
+++ jdxi_editor/midi/preset/type.py
@@ -1,4 +1,16 @@
-from enum import Enum
+"""
+Module: preset_type
+
+This module defines the `PresetType` class, which categorizes JD-Xi presets
+and provides MIDI area codes for different synth sections.
+
+Classes:
+    - PresetType: Defines preset categories and their corresponding MIDI area codes.
+
+Methods:
+    - get_area_code(preset_type): Returns the MIDI area code for a given preset type.
+
+"""
 
 
 class PresetType():
@@ -17,4 +29,4 @@ class PresetType():
             PresetType.DIGITAL_2: 0x19,
             PresetType.DRUMS: 0x23
         }
-        return area_codes.get(preset_type) 
+        return area_codes.get(preset_type)
