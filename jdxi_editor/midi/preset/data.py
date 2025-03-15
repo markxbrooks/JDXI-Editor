346e111d52d6b86aab5036488c241e2552274329
Mark Brooks <Mark.x.brooks@gmail.com>
2025-03-14 22:16:38 -0400

    added PresetData type

--- jdxi_editor/midi/preset/data.py
+++ jdxi_editor/midi/preset/data.py
@@ -1,3 +1,17 @@
+"""
+Module: preset_data
+
+This module defines the `PresetData` dataclass, which represents the structure of a JD-Xi preset.
+It includes attributes for preset type, current selection, modification status, and MIDI channel.
+
+Classes:
+    - PresetData: Stores preset metadata for MIDI program selection and management.
+
+Constants:
+    - MIDI_CHANNEL_DIGITAL1: Default MIDI channel for Digital 1 presets.
+    - PresetType.DIGITAL_1: Default preset type for Digital 1.
+"""
+
 from dataclasses import dataclass
 
 from jdxi_editor.midi.data.constants import MIDI_CHANNEL_DIGITAL1
