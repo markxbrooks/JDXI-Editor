194c8f35b884c0c699375f8097b5f4c718bed49e
Mark Brooks <Mark.x.brooks@gmail.com>
2025-03-28 10:09:59 -0400

    combining PrestHelper and PresetHandler

--- jdxi_editor/midi/preset/helper.py
+++ jdxi_editor/midi/preset/helper.py
@@ -34,10 +34,28 @@ from pubsub import pub
 
 from PySide6.QtCore import Signal, QObject
 
+from jdxi_editor.midi.data.constants.constants import MIDI_CHANNEL_ANALOG, MIDI_CHANNEL_DRUMS
+from jdxi_editor.midi.data.programs.analog import ANALOG_PRESET_LIST
+from jdxi_editor.midi.data.programs.drum import DRUM_KIT_LIST
+from jdxi_editor.midi.data.programs.presets import DIGITAL_PRESET_LIST
 from jdxi_editor.midi.message.roland import RolandSysEx
 from jdxi_editor.midi.preset.type import SynthType
 from jdxi_editor.midi.sysex.requests import PROGRAM_TONE_NAME_PARTIAL_REQUESTS
 from jdxi_editor.ui.editors.helpers.program import get_preset_parameter_value, log_midi_info
+
+
+def get_preset_values(preset_index, preset_list=DIGITAL_PRESET_LIST):
+    """Retrieve MSB, LSB, and PC values for a given preset."""
+    msb = get_preset_parameter_value("msb", preset_index, preset_list)
+    lsb = get_preset_parameter_value("lsb", preset_index, preset_list)
+    pc = get_preset_parameter_value("pc", preset_index, preset_list)
+
+    if None in [msb, lsb, pc]:
+        logging.error(f"Could not retrieve preset parameters for program {preset_index}")
+        return None, None, None
+
+    logging.info(f"Retrieved MSB, LSB, PC: {msb}, {lsb}, {pc}")
+    return msb, lsb, pc
 
 
 class PresetHelper(QObject):
@@ -86,16 +104,17 @@ class PresetHelper(QObject):
         """Get the available presets."""
         return self.presets
 
-    def save_preset(self, program_number: int, params):
-        """Save the current preset to the preset list."""
-        name = self.presets[program_number]
-        print(f"name: \t{name}")
-        print(f"params: \t{params}")
-        self.preset_changed.emit(self.current_preset_zero_indexed, self.channel)
-        self.update_display.emit(self.type, self.current_preset_zero_indexed, self.channel)
-        return self.get_current_preset()
-
     def load_preset_by_program_change(self, preset_index):
+        """Load a preset using program change."""
+        logging.info(f"Preset index: {preset_index}")
+
+        msb, lsb, pc = get_preset_values(preset_index)
+        if None in [msb, lsb, pc]:
+            return
+
+        self.send_program_change(self.channel, msb, lsb, pc)
+
+    def load_preset_by_program_change_old(self, preset_index):
         """Load a preset by program change."""
         logging.info(f"preset_index : {preset_index}")
 
@@ -113,49 +132,40 @@ class PresetHelper(QObject):
 
         # Send bank select and program change
         # Note: PC is 0-based in MIDI, so subtract 1
-        self.midi_helper.send_bank_select_and_program_change(
-            self.channel,  # MIDI channel
-            msb,  # MSB is already correct
-            lsb,  # LSB is already correct
-            pc - 1  # Convert 1-based PC to 0-based
-        )
+        self.send_program_change(self.channel, msb, lsb, pc)
         self.data_request()
 
     def load_preset(self, preset_data):
         """Load the preset based on the provided data."""
         logging.info(f"Loading preset: {preset_data}")
         program_number, channel = preset_data.current_selection, preset_data.channel
-        if channel == MIDI_CHANNEL_DRUMS:
-            preset_list = DRUM_KIT_LIST
-        elif channel == MIDI_CHANNEL_ANALOG:
-            preset_list = ANALOG_PRESET_LIST
-        else:
-            preset_list = DIGITAL_PRESET_LIST
-            # Get MSB, LSB, PC values from the preset using get_preset_parameter_value
-        msb = get_preset_parameter_value("msb", program_number, preset_list)
-        lsb = get_preset_parameter_value("lsb", program_number, preset_list)
-        pc = get_preset_parameter_value("pc", program_number, preset_list)
 
+        # Select the correct preset list based on the channel
+        preset_list = {
+            MIDI_CHANNEL_DRUMS: DRUM_KIT_LIST,
+            MIDI_CHANNEL_ANALOG: ANALOG_PRESET_LIST
+        }.get(channel, DIGITAL_PRESET_LIST)
+
+        msb, lsb, pc = get_preset_values(program_number, preset_list)
         if None in [msb, lsb, pc]:
-            logging.error(f"Could not retrieve preset parameters for program {program_number}")
             return
 
-        logging.info(f"retrieved msb, lsb, pc : {msb}, {lsb}, {pc}")
-        log_midi_info(msb, lsb, pc)
-
-        # Send bank select and program change
-        # Note: PC is 0-based in MIDI, so subtract 1
-        self.midi_helper.send_bank_select_and_program_change(
-            channel,  # MIDI channel
-            msb,  # MSB is already correct
-            lsb,  # LSB is already correct
-            pc - 2  # Convert 1-based PC to 0-based
-        )
-        self.data_request()
+        self.send_program_change(channel, msb, lsb, pc)
 
     def data_request(self):
         for midi_request in self.midi_requests:
             byte_list_message = bytes.fromhex(midi_request)
             self.midi_helper.send_raw_message(byte_list_message)
 
+    def send_program_change(self, channel, msb, lsb, pc):
+        """Send a Bank Select and Program Change message."""
+        log_midi_info(msb, lsb, pc)
+
+        if pc is None:
+            logging.error("Program Change value is None, aborting.")
+            return
+
+        # Convert 1-based PC to 0-based
+        self.midi_helper.send_bank_select_and_program_change(channel, msb, lsb, pc - 1)
+        self.data_request()
 
