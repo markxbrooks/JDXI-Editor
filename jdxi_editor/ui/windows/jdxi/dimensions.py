d6b9e18999f036a532885d48bd571d916ca38e72
Brooks, Mark <mark.brooks@evotec.com>
2025-04-07 16:47:08 -0400

    made digital display larger

--- jdxi_editor/ui/windows/jdxi/dimensions.py
+++ jdxi_editor/ui/windows/jdxi/dimensions.py
@@ -20,7 +20,7 @@ class JDXIDimensions:
     MARGIN = 15
 
     # LED display area (enlarged for 2 rows)
-    DISPLAY_WIDTH = 180
+    DISPLAY_WIDTH = 190
     DISPLAY_HEIGHT = 70
     DISPLAY_X = MARGIN + 20
     DISPLAY_Y = MARGIN + 35
