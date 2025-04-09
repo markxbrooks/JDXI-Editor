63c121cbe051f979eac467fe8b7adc1de04fc8e6
Mark Brooks <mark.x.brooks@gmail.com>
2025-04-09 08:36:25 -0400

    Update data.py

--- jdxi_editor/midi/preset/data.py
+++ jdxi_editor/midi/preset/data.py
@@ -26,3 +26,50 @@ class Preset:
     modified: int = 0
     channel: int = MidiChannel.MIDI_CHANNEL_DIGITAL1
     name: Optional[str] = None
+
+
+class PresetData:
+    """
+    A class to handle synth data, including preset selection based on synth type.
+    """
+
+    @staticmethod
+    def get_preset_details(synth_type, preset_num):
+        """
+        Get preset details based on the synth type and preset number.
+
+        :param synth_type: The type of synth (e.g., SynthType.ANALOG, SynthType.DIGITAL_1, etc.)
+        :param preset_num: The preset number
+        :return: A dictionary with presets, bank_msb, bank_lsb, and program
+        """
+        if synth_type == SynthType.ANALOG:
+            presets = AN_PRESETS
+            bank_msb = 0
+            bank_lsb = preset_num // 7
+            program = preset_num % 7
+        elif synth_type == SynthType.DIGITAL_1:
+            presets = DIGITAL_PRESETS_ENUMERATED
+            bank_msb = 1
+            bank_lsb = preset_num // 16
+            program = preset_num % 16
+        elif synth_type == SynthType.DIGITAL_2:
+            presets = DIGITAL_PRESETS_ENUMERATED
+            bank_msb = 2
+            bank_lsb = preset_num // 16
+            program = preset_num % 16
+        else:  # Drums
+            presets = DRUM_PRESETS_ENUMERATED
+            bank_msb = 3
+            bank_lsb = preset_num // 16
+            program = preset_num % 16
+
+        return {
+            'presets': presets,
+            'bank_msb': bank_msb,
+            'bank_lsb': bank_lsb,
+            'program': program
+        }
+
+# Example usage:
+# synth_data = SynthData.get_preset_details(SynthType.DIGITAL_1, 10)
+# print(synth_data)
