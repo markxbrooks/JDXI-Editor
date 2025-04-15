0c44fc2a6050a2709b2cafc0c10f3052792c6a35
Mark Brooks <Mark.x.brooks@gmail.com>
2025-04-14 21:00:41 -0400

    updating ProgramCommonParameterMessage

--- jdxi_editor/midi/message/program/common.py
+++ jdxi_editor/midi/message/program/common.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 from jdxi_editor.midi.data.address.address import CommandID, AddressMemoryAreaMSB, AddressOffsetSystemLMB, \
-    AddressOffsetProgramLMB, ZERO_BYTE
+    AddressOffsetProgramLMB, ZERO_BYTE, AddressOffsetSystemUMB
 from jdxi_editor.midi.message.roland import RolandSysEx
 
 
@@ -11,8 +11,8 @@ class ProgramCommonParameterMessage(RolandSysEx):
 
     command: int = CommandID.DT1
     address_msb: int = AddressMemoryAreaMSB.PROGRAM  # 0x18: Program area
-    address_umb: int = AddressOffsetSystemLMB.COMMON  # 0x00: Common section
-    address_lmb: int = AddressOffsetProgramLMB.COMMON  # Always 0x00
+    address_umb: int = AddressOffsetSystemUMB.COMMON  # 0x00: Common section
+    address_lmb: int = AddressOffsetSystemLMB.COMMON  # Always 0x00
     address_lsb: int = ZERO_BYTE  # Parameter number
     value: int = ZERO_BYTE  # Parameter value
 
