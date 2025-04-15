81785b1cd52fe00ea06bc15ed5f212c6de8ac74d
Mark Brooks <mark.x.brooks@gmail.com>
2025-04-15 07:12:27 -0400

    Update main.py

--- jdxi_editor/main.py
+++ jdxi_editor/main.py
@@ -94,6 +94,15 @@ def resource_path(relative_path):
     base_path = getattr(sys, '_MEIPASS', os.path.abspath("."))
     return os.path.join(base_path, relative_path)
 
+
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+
 def main():
     try:
         # Set up logging first
