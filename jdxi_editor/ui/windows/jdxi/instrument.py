7e525a1c5754cbbce031a26a19936f352d1eb0a5
Mark Brooks <mark.x.brooks@gmail.com>
2025-04-19 08:50:06 -0400

    Update instrument.py

--- jdxi_editor/ui/windows/jdxi/instrument.py
+++ jdxi_editor/ui/windows/jdxi/instrument.py
@@ -369,7 +369,7 @@ class JdxiInstrument(JdxiUi):
         return presets
 
     def _previous_program(self):
-        """Decrement the tone index and update the display."""
+        """Decrement the program index and update the display."""
         if self.current_program_number == 1:
             logging.info("Already at the first program.")
             msg_box = QMessageBox()
