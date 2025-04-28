b24d0b9d913701754df814c25697785d1fc8dad0
Brooks, Mark <mark.brooks@evotec.com>
2025-04-28 18:54:01 -0400

    working on save_all_controls_to_single_file

--- jdxi_editor/ui/io/controls.py
+++ jdxi_editor/ui/io/controls.py
@@ -11,10 +11,10 @@ def save_all_controls_to_single_file(editors, file_path):
         file_path (str): The file path where the combined JSON data will be saved.
     """
     try:
-        combined_data = {}
-
+        combined_data = {"JD_XI_HEADER": "f041100000000e"}
         for editor in editors:
-            editor_part = editor.umb.name  # Get the part of the editor class
+            combined_data["ADDRESS"] = editor.sysex_address
+            editor_part = editor.sysex_address.umb.name  # Get the part of the editor class
             combined_data[editor_part] = editor.get_controls_as_dict()
 
         # Save the combined data to a single JSON file
