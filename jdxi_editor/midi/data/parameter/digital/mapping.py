a12b40ffcc209e578a0ccb4806dc9607a0a4e8a3
Brooks, Mark <mark.brooks@evotec.com>
2025-04-29 20:23:25 -0400

    refactoring ADSR widget

---
+++ jdxi_editor/midi/data/parameter/digital/mapping.py
@@ -0,0 +1,10 @@
+ENVELOPE_MAPPING = {
+    "FILTER_ENV_ATTACK_TIME": "attack_time",
+    "FILTER_ENV_DECAY_TIME": "decay_time",
+    "FILTER_ENV_SUSTAIN_LEVEL": "sustain_level",
+    "FILTER_ENV_RELEASE_TIME": "release_time",
+    "AMP_ENV_ATTACK_TIME": "attack_time",
+    "AMP_ENV_DECAY_TIME": "decay_time",
+    "AMP_ENV_SUSTAIN_LEVEL": "sustain_level",
+    "AMP_ENV_RELEASE_TIME": "release",
+}
