bb0e90ae2bfa31fe34a736e98547f9c673b60434
Mark Brooks <Mark.x.brooks@gmail.com>
2025-05-02 09:26:26 -0400

    updating logging

--- jdxi_editor/ui/editors/helpers/analog.py
+++ jdxi_editor/ui/editors/helpers/analog.py
@@ -1,12 +1,13 @@
-import logging
+""" Helper function for the AnalogParameterEditor."""
 
 from jdxi_editor.log.message import log_message
+from jdxi_editor.log.parameter import log_parameter
 from jdxi_editor.midi.data.parameter.analog import AddressParameterAnalog
 
 
-def get_analog_parameter_by_address(address: int):
+def get_analog_parameter_by_address(address: tuple[int]):
     """Retrieve the DigitalParameter by its address."""
-    log_message(f"address: {address}")
+    log_parameter("address", address)
     for param in AddressParameterAnalog:
         if param.address == address:
             log_message(f"get_analog_parameter_by_address found param: {param}")
--- jdxi_editor/ui/editors/synth/editor.py
+++ jdxi_editor/ui/editors/synth/editor.py
@@ -25,7 +25,7 @@ import os
 import logging
 from typing import Optional, Any
 from PySide6.QtGui import QPixmap, QKeySequence, QShortcut
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QGroupBox
 from PySide6.QtCore import Qt, Signal
 
 from jdxi_editor.log.error import log_error
@@ -187,6 +187,40 @@ class SynthEditor(SynthBase):
             "midi_channel",
         ]:
             setattr(self, attr, getattr(self.synth_data, attr))
+
+    def _create_instrument_preset_group(self, synth_type: str = "Analog") -> QGroupBox:
+        raise NotImplementedError("Should be implemented by subclass")
+
+    """def _create_instrument_preset_group(self, synth_type: str = "Analog") -> QGroupBox:
+        ""
+        Create the instrument preset group box.
+        :param synth_type: str
+        :return: QGroupBox
+        ""
+        instrument_preset_group = QGroupBox(f"{synth_type} Synth")
+        instrument_title_group_layout = QVBoxLayout(instrument_preset_group)
+        self.instrument_title_label = DigitalTitle()
+        instrument_title_group_layout.addWidget(self.instrument_title_label)
+        self.read_request_button = QPushButton("Send Read Request to Synth")
+        self.read_request_button.clicked.connect(self.data_request)
+        instrument_title_group_layout.addWidget(self.read_request_button)
+        self.instrument_selection_label = QLabel(f"Select an {synth_type} synth:")
+        instrument_title_group_layout.addWidget(self.instrument_selection_label)
+        self.instrument_selection_combo = PresetComboBox(self.preset_list)
+        self.instrument_selection_combo.setStyleSheet(JDXIStyle.COMBO_BOX_ANALOG)
+        self.instrument_selection_combo.combo_box.setEditable(True)
+        self.instrument_selection_combo.combo_box.currentIndexChanged.connect(
+            self.update_instrument_image
+        )
+        self.instrument_selection_combo.combo_box.currentIndexChanged.connect(
+            self.update_instrument_title
+        )
+        self.instrument_selection_combo.load_button.clicked.connect(
+            self.update_instrument_preset
+        )
+        self.instrument_selection_combo.preset_loaded.connect(self.load_preset)
+        instrument_title_group_layout.addWidget(self.instrument_selection_combo)
+        return instrument_preset_group"""
         
     def get_controls_as_dict(self):
         """
