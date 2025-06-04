0f42e46000a9dd0f6da8c2cbf7dd9e307928713e
Mark Brooks <Mark.x.brooks@gmail.com>
2025-06-04 09:23:13 -0400

    adding drum tooltips

--- jdxi_editor/midi/data/parameter/digital/modify.py
+++ jdxi_editor/midi/data/parameter/digital/modify.py
@@ -35,18 +35,23 @@ class AddressParameterDigitalModify(AddressParameter):
     These parameters are shared across all partials.
     """
 
-    def __init__(self, address: int, min_val: int, max_val: int):
+    def __init__(self,
+                 address: int,
+                 min_val: int,
+                 max_val: int,
+                 tooltip: str = ""):
         super().__init__(address, min_val, max_val)
         self.address = address
         self.min_val = min_val
         self.max_val = max_val
+        self.tooltip = tooltip
 
-    ATTACK_TIME_INTERVAL_SENS = (0x01, 0, 127)
-    RELEASE_TIME_INTERVAL_SENS = (0x02, 0, 127)
-    PORTAMENTO_TIME_INTERVAL_SENS = (0x03, 0, 127)
-    ENVELOPE_LOOP_MODE = (0x04, 0, 2)
-    ENVELOPE_LOOP_SYNC_NOTE = (0x05, 0, 19)
-    CHROMATIC_PORTAMENTO = (0x06, 0, 1)
+    ATTACK_TIME_INTERVAL_SENS = (0x01, 0, 127, "Shortens the FILTER and AMP Attack Time according to the spacing between note-on events.\nHigher values produce a greater effect. With a setting of 0, there will be no effect.\nThis is effective when you want to play rapid notes using a sound that has a slow attack\n(Attack Time).")
+    RELEASE_TIME_INTERVAL_SENS = (0x02, 0, 127, "Shortens the FILTER and AMP Release Time if the interval between one note-on and the next\nnote-off is brief. Higher values produce a greater effect. With a setting of 0, there will be no effect.\nThis is effective when you want to play staccato notes using a sound that has a slow release")
+    PORTAMENTO_TIME_INTERVAL_SENS = (0x03, 0, 127, "Shortens the Portamento Time according to the spacing between note-on events. Higher values\nproduce a greater effect. With a setting of 0, there will be no effect.")
+    ENVELOPE_LOOP_MODE = (0x04, 0, 2, "Use this to loop the envelope between certain regions during a note-on.\nOFF The envelope will operate normally.\nFREE-RUN When the Decay segment has ended, the envelope will return to the Attack. The Attack through\nDecay segments will repeat until note-off occurs.\nTEMPO-SYNC Specifies the loop rate as a note value (Sync Note parameter).")
+    ENVELOPE_LOOP_SYNC_NOTE = (0x05, 0, 19, "Returns to the Attack at the specified rate. If the Attack+Decay time is shorter than the specified\nloop, the sound is maintained at the Sustain Level. If the Attack+Decay time is longer than the\nspecified loop, the sound returns to the Attack even if the Decay has not completed. This will\n\ncontinue repeating until note-off occurs." )
+    CHROMATIC_PORTAMENTO = (0x06, 0, 1, "If this is turned ON, portamento will operate in semitone steps. If this is turned OFF, the pitch will\nchange smoothly from one note to the next.\n This is effective when you want to play chromatic portamento\n using a sound that has a\nslow portamento time.")
 
     def get_switch_text(self, value: int) -> str:
         """Get display text for switch values"""
