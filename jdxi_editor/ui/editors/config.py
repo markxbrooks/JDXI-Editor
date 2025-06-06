b6c47d8a3860c8a0a0a45839e9dfc8a2c775b08a
Mark Brooks <Mark.x.brooks@gmail.com>
2025-06-06 09:19:00 -0400

    adding icons to main editor tab widget

---
+++ jdxi_editor/ui/editors/config.py
@@ -0,0 +1,32 @@
+"""
+Configuration of Editor classes for the JDXI Editor.
+
+Stores
+-Title: The title of the editor.
+-Editor Class: The class that implements the editor functionality.
+-Synth Type: Optional type of synthesizer associated with the editor.
+-MIDI Channel: Optional MIDI channel for the editor.
+-icon: Icon for the editor, represented as a string.
+-Keyword Arguments: Additional parameters for the editor.
+
+
+Example usage:
+"arpeggio": EditorConfig(
+    title="Arpeggiator",
+    editor_class=ArpeggioEditor,
+    icon="ph.music-notes-simple-bold"
+),
+"""
+
+from dataclasses import dataclass, field
+from typing import Type, Optional, Any, Dict
+
+
+@dataclass
+class EditorConfig:
+    title: str
+    editor_class: Type
+    synth_type: Optional[Any] = None
+    midi_channel: Optional[Any] = None
+    kwargs: Dict[str, Any] = field(default_factory=dict)
+    icon: str = ""
