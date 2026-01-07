18d2dfed6ed77671f5d7354e51d4492283275bb3
Mark Brooks <mark.x.brooks@gmail.com>
2026-01-06 20:39:53 -0500

    [release] v0.9.1

--- jdxi_editor/project.py
+++ jdxi_editor/project.py
@@ -1,5 +1,5 @@
 """Project metadata and version information for JD-Xi Editor."""
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 __program__ = "JD-Xi Editor"
 __project__ = "JD-Xi Editor"
 __author__ = "JDXi Editor Team"
