49337d12101257ebf2eb81cbd773f7be8d345825
Mark Brooks <Mark.x.brooks@gmail.com>
2026-01-08 01:05:12 -0500

    [refactor] splitting out picomidi

--- picomidi/cc/bank.py
+++
@@ -1,3 +0,0 @@
-class BankSelect:
-    MSB = 0x00  # CC#0: Bank Select MSB (standard MIDI)
-    LSB = 0x20  # CC#32: Bank Select LSB (standard MIDI)
--- picomidi/parser/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-MIDI Message Parser
-
-This module provides functionality for parsing raw MIDI bytes
-into message objects.
-"""
-
-__all__ = []
