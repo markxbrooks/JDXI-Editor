0771b6dd232b24f85ba54f77be007c5c00fca062
Mark Brooks <Mark.x.brooks@gmail.com>
2026-01-10 15:38:14 -0500

    [+objects] parameters for address maps

--- jdxi_editor/midi/message/jdxi.py
+++ jdxi_editor/midi/message/jdxi.py
@@ -12,9 +12,10 @@ Usage Example:
     >>> from jdxi_editor.midi_state.data.address.address import ModelID
     >>> from jdxi_editor.midi_state.data.address.SYSEX import RolandID
     >>> JD_XI_MODEL_ID
-    [ModelID.MODEL_ID_1, ModelID.MODEL_ID_2, ModelID.MODEL_ID_3, ModelID.MODEL_ID_4]
-    >>> JD_XI_HEADER_LIST
-    [RolandID.ROLAND_ID, RolandID.DEVICE_ID, *JD_XI_MODEL_ID]
+    [<ModelID.MODEL_ID_1: 0x00>, <ModelID.MODEL_ID_1: 0x00>, <ModelID.MODEL_ID_1: 0x00>, <ModelID.MODEL_ID_4: 0x0E>]
+    >>> JDXiSysexHeader.to_list()
+        [<RolandID.ROLAND_ID: 65>, <RolandID.DEVICE_ID: 16>, <ModelID.MODEL_ID_1: 0x00>, <ModelID.MODEL_ID_1: 0x00>, <ModelID.MODEL_ID_1: 0x00>, <ModelID.MODEL_ID_4: 0x0E>]
+
 
 """
 
@@ -28,3 +29,18 @@ JD_XI_MODEL_ID = [
     ModelID.MODEL_ID_4,
 ]
 JD_XI_HEADER_LIST = [RolandID.ROLAND_ID, RolandID.DEVICE_ID, *JD_XI_MODEL_ID]
+
+
+class JDXiSysexHeader:
+    """
+    JD-Xi System Exclusive Message Header
+    """
+    ID = RolandID
+    MODEL = ModelID
+
+    @classmethod
+    def to_list(cls) -> list[int]:
+        """
+        Convert the header to a list of integers
+        """
+        return [cls.ID.ROLAND_ID, cls.ID.DEVICE_ID, cls.MODEL.MODEL_ID_1, cls.MODEL.MODEL_ID_2, cls.MODEL.MODEL_ID_3, cls.MODEL.MODEL_ID_4]
