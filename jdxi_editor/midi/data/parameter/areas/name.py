40ebbb4daf843e5b912e0bd726dbd8b13e10eff4
Mark Brooks <Mark.x.brooks@gmail.com>
2026-01-10 13:02:10 -0500

    [fix] PARAMETER_ADDRESS_MAP

---
+++ jdxi_editor/midi/data/parameter/areas/name.py
@@ -0,0 +1,11 @@
+from enum import Enum, auto
+
+
+class ParameterAreas(Enum):
+    """Enumerates parameter areas"""
+    SYSTEM = auto()
+    TEMPORARY_TONE = auto()
+    PROGRAM = auto()
+    SUPERNATURAL_SYNTH_TONE = auto()
+    ANALOG_SYNTH_TONE = auto()
+    DRUM_KIT = auto()
