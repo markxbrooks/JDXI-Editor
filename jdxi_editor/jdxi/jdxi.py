77162a314d74145e20dbb47610480ee747e84b19
Mark Brooks <mark.x.brooks@gmail.com>
2026-01-17 10:03:41 -0500

    Update jdxi.py

--- jdxi_editor/jdxi/jdxi.py
+++ jdxi_editor/jdxi/jdxi.py
@@ -8,7 +8,7 @@ from jdxi_editor.ui.windows.jdxi.dimensions import JDXiDimensions
 
 class JDXi:
     """Composition of main JDXI UI components as a single, immutable container."""
-    STYLE: JDXiStyle = JDXiStyle # if JDXiStyle is a class
+    STYLE: JDXiStyle = JDXiStyle 
     THEME: JDXiThemeManager = JDXiThemeManager
     ICONS: IconRegistry = IconRegistry
     DIMENSIONS: JDXiDimensions = JDXiDimensions
