25a157963ee1ed3c57b6ea0adfa1d03555061b77
Mark Brooks <mark.x.brooks@gmail.com>
2026-01-17 10:03:16 -0500

    Update jdxi.py

--- jdxi_editor/jdxi/jdxi.py
+++ jdxi_editor/jdxi/jdxi.py
@@ -7,8 +7,8 @@ from jdxi_editor.jdxi.style.icons import IconRegistry
 from jdxi_editor.ui.windows.jdxi.dimensions import JDXiDimensions
 
 class JDXi:
-    """ Composition of main JDXI UI components"""
-    STYLE: JDXiStyle
-    THEME: JDXiThemeManager
-    ICONS: IconRegistry
-    DIMENSIONS: JDXiDimensions
+    """Composition of main JDXI UI components as a single, immutable container."""
+    STYLE: JDXiStyle = JDXiStyle # if JDXiStyle is a class
+    THEME: JDXiThemeManager = JDXiThemeManager
+    ICONS: IconRegistry = IconRegistry
+    DIMENSIONS: JDXiDimensions = JDXiDimensions
