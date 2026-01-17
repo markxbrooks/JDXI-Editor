f541d7538c0f0d6d4b10cfebd82fc3f49fb5773e
Mark Brooks <mark.x.brooks@gmail.com>
2026-01-17 09:55:29 -0500

    Create jdxi.py

---
+++ jdxi_editor/jdxi/jdxi.py
@@ -0,0 +1,10 @@
+from jdxi_editor.jdxi.style import JDXiStyle, JDXiThemeManager
+from jdxi_editor.jdxi.style.icons import IconRegistry
+from jdxi_editor.ui.windows.jdxi.dimensions import JDXiDimensions
+
+class JDXi:
+    """ Composition of main JDXI UI components"""
+    STYLE: JDXiStyle
+    THEME: JDXiThemeManager
+    ICONS: IconRegistry
+    DIMENSIONS: JDXiDimensions
