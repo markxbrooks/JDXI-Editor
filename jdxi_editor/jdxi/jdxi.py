914c07d64df68dcf06c7039e4e40cee82e13d425
Mark Brooks <mark.x.brooks@gmail.com>
2026-01-18 00:08:49 -0500

    Update jdxi.py

--- jdxi_editor/jdxi/jdxi.py
+++ jdxi_editor/jdxi/jdxi.py
@@ -4,10 +4,11 @@ JDXi Composition God Object (!)
 
 from jdxi_editor.jdxi.jdxiui import JDXiUI
 from jdxi_editor.jdxi.midi.constant import JDXiMidi
+from jdxi_editor.jdxi.synth.type import JDXiSynth
 
 
 class JDXi:
     """Composition of main JDXI UI components as a single, immutable container."""
-
+    Synth: JDXiSynth = JDXiSynth
     UI: JDXiUI = JDXiUI
     Midi: JDXiMidi = JDXiMidi
