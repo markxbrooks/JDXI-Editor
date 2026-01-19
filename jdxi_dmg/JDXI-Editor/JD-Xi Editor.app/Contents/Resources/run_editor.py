cfd1a9e9852a39768c5e1e3e53f450804360af6f
Mark Brooks <Mark.x.brooks@gmail.com>
2026-01-19 18:31:23 -0500

    [ui] fixing preset item editing

--- jdxi_dmg/JDXI-Editor/JD-Xi Editor.app/Contents/Resources/run_editor.py
+++
@@ -1,7 +0,0 @@
-# run_editor.py
-import runpy
-
-if __name__ == "__main__":
-    from jdxi_editor.main import main
-
-    main()
--- jdxi_editor/project.py
+++ jdxi_editor/project.py
@@ -1,6 +1,6 @@
 """Project metadata and version information for JD-Xi Editor."""
 
-__version__ = "0.9.1"
+__version__ = "0.9.2"
 __program__ = "JD-Xi Editor"
 __project__ = "JD-Xi Editor"
 __author__ = "JDXi Editor Team"
