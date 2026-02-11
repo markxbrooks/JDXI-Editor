dd3c46c2f06436aabc451dc6972e17d6117915a4
Mark Brooks <mark.x.brooks@gmail.com>
2026-02-11 07:57:50 -0500

    Update widget.py

--- jdxi_editor/ui/editors/digital/partial/amp/widget.py
+++ jdxi_editor/ui/editors/digital/partial/amp/widget.py
@@ -2,9 +2,10 @@ from dataclasses import dataclass
 from typing import List, Optional
 
 from PySide6.QtWidgets import QTabWidget, QWidget
+from jdxi_editor.ui.editors.base.amp.widget import AmpWidgets
 
 
 @dataclass
-class DigitalAmpWidgets[AmpWidgets]:
+class DigitalAmpWidgets(AmpWidgets):
     """Single container for all amp UI widgets (Analog and Digital)."""
     pan: Optional[List[QWidget]] = None
