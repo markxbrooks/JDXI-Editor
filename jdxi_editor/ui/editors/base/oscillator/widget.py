c550e87164bbfffa379cdfd77a0aec158e0c9142
Mark Brooks <mark.x.brooks@gmail.com>
2026-02-11 07:26:04 -0500

    Update widget.py

--- jdxi_editor/ui/editors/base/oscillator/widget.py
+++ jdxi_editor/ui/editors/base/oscillator/widget.py
@@ -14,22 +14,10 @@ from PySide6.QtWidgets import QWidget
 
 @dataclass
 class OscillatorWidgets:
-    """All oscillator widgets in one place (Analog and Digital)."""
-
-    # Common
+    """Common oscillator widgets in one place to be extended (by Analog and Digital)."""
     waveform_buttons: dict[Any, QWidget] | None = None
     pitch_env_widget: QWidget | None = None
     pwm_widget: QWidget | None = None
     switches: list[QWidget] | None = field(default_factory=list)
     tuning: list[QWidget] | None = field(default_factory=list)
     env: list[QWidget] | None = field(default_factory=list)
-    # Analog-specific
-    # sub_oscillator_type_switch: QWidget | None = None
-    # osc_pitch_env_velocity_sensitivity_slider: QWidget | None = None
-    # osc_pitch_coarse_slider: QWidget | None = None
-    # osc_pitch_fine_slider: QWidget | None = None
-    # pitch_env_widgets: list[QWidget] | None = field(default_factory=list)
-    # Digital-specific
-    # pcm_wave: QWidget | None = None
-    # pw_shift_slider: QWidget | None = None
-    # super_saw_detune: QWidget | None = None
