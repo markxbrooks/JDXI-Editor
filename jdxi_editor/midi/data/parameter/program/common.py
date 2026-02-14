0905a86bc2cd75c5d5a4acd7b770c1fec676cbf4
Mark Brooks <mark.x.brooks@gmail.com>
2026-02-14 02:17:07 -0500

    Update common.py

--- jdxi_editor/midi/data/parameter/program/common.py
+++ jdxi_editor/midi/data/parameter/program/common.py
@@ -51,6 +51,8 @@ class SystemCommonParam(AddressParameter):
         display_max: Optional[int] = None,
         tooltip: Optional[str] = None,
         display_name: Optional[str] = None,
+        options: Optional[list] = None,
+        values: Optional[list] = None
     ):
         super().__init__(address, min_val, max_val)
         self.display_min = display_min if display_min is not None else min_val
